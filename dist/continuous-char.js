0e0767a7b5078e25c7a7e46ddbe5d6b0547f1578
liuminxia <liuminxia@tal.com>
2022-06-17 18:00:10 +0800

    feat: 获取字符串中连续最多的字符以及次数-使用双指针

--- dist/continuous-char.js
+++ dist/continuous-char.js
@@ -1,6 +1,3 @@
-/**
- * @description 获取字符串中连续最多的字符以及次数
- */
 const findContinuousChar1 = (str) => {
     const res = {
         char: '',
@@ -32,8 +29,43 @@ const findContinuousChar1 = (str) => {
     }
     return res;
 };
+/**
+* @description 获取字符串中连续最多的字符以及次数 - 双指针O(n)
+*/
+const findContinuousChar = (str) => {
+    const res = {
+        char: '',
+        length: 0
+    };
+    const length = str.length;
+    if (length === 0)
+        return res;
+    let tempLength = 0;
+    let i = 0;
+    let j = 0;
+    for (i; i < length; i++) {
+        if (str[i] === str[j]) {
+            tempLength++;
+        }
+        if (str[i] !== str[j] || i === length - 1) {
+            // 不相等，或者 i 到了字符串的末尾
+            if (tempLength > res.length) {
+                res.char = str[j];
+                res.length = tempLength;
+            }
+            tempLength = 0; //重置
+            // 没有到末尾
+            if (i < length - 1) {
+                j = i; // 让j追上i
+                i--;
+            }
+        }
+    }
+    return res;
+};
 // 功能测试
 const testStr = 'aaabbcccdded1123';
-const res = findContinuousChar1(testStr); // {char: 'a', length: 3}
-console.log(res);
-export default findContinuousChar1;
+const res1 = findContinuousChar1(testStr); // {char: 'a', length: 3}
+const res = findContinuousChar(testStr); // {char: 'a', length: 3}
+console.log(res1, res);
+export { findContinuousChar1, findContinuousChar };
--- src/continuous-char.ts
+++ src/continuous-char.ts
@@ -1,7 +1,6 @@
 /**
- * @description 获取字符串中连续最多的字符以及次数
+ * @description 获取字符串中连续最多的字符以及次数 - 嵌套循环O(n)
  */
-
  interface IRes {
    char: string
    length: number
@@ -44,10 +43,54 @@
    return res
  }
 
+ /**
+ * @description 获取字符串中连续最多的字符以及次数 - 双指针O(n)
+ */
+const findContinuousChar = (str: string):IRes =>{
+  const res:IRes = {
+    char: '',
+    length: 0
+  }
+
+  const length = str.length
+  if(length === 0) return res
+
+  
+  let tempLength = 0
+  let i = 0
+  let j = 0
+  for (i; i < length; i++) {
+    if(str[i] === str[j]){
+      tempLength++
+    }
+
+    if(str[i] !== str[j] || i === length - 1){
+      // 不相等，或者 i 到了字符串的末尾
+      if(tempLength > res.length){
+        res.char = str[j]
+        res.length = tempLength
+      }
+
+      tempLength = 0 //重置
+
+      // 没有到末尾
+      if(i < length - 1){
+        j = i // 让j追上i
+        i--
+      }
+      
+    }
+  }
+
+  return res
+}
+
+
  // 功能测试
  const testStr = 'aaabbcccdded1123'
- const res = findContinuousChar1(testStr) // {char: 'a', length: 3}
- console.log(res);
+ const res1 = findContinuousChar1(testStr) // {char: 'a', length: 3}
+ const res = findContinuousChar(testStr) // {char: 'a', length: 3}
+ console.log(res1,res);
 
- export default findContinuousChar1
+ export {findContinuousChar1, findContinuousChar}
  
