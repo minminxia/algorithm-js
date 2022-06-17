d8d88134c2ef09526bdd731beb059cc12ee492a6
liuminxia <liuminxia@tal.com>
2022-06-17 17:32:22 +0800

    feat: 获取字符串中连续最多的字符以及次数-使用嵌套循环

---
+++ dist/continuous-char.js
@@ -0,0 +1,39 @@
+/**
+ * @description 获取字符串中连续最多的字符以及次数
+ */
+const findContinuousChar1 = (str) => {
+    const res = {
+        char: '',
+        length: 0
+    };
+    const length = str.length;
+    if (length === 0)
+        return res;
+    let tempLength = 0; // 临时记录当前连续字符串的长度
+    for (let i = 0; i < length; i++) {
+        tempLength = 0; // 重置
+        for (let j = i; j < length; j++) {
+            if (str[i] === str[j]) {
+                tempLength++;
+            }
+            // aaabbcccdded
+            if (str[i] !== str[j] || j === length - 1) {
+                // 不相等，或者已经到了最后一个元素，要去判断最大值
+                if (tempLength > res.length) {
+                    res.char = str[i];
+                    res.length = tempLength;
+                }
+                if (i < length - 1) {
+                    i = j - 1; // 跳步
+                }
+                break;
+            }
+        }
+    }
+    return res;
+};
+// 功能测试
+const testStr = 'aaabbcccdded1123';
+const res = findContinuousChar1(testStr); // {char: 'a', length: 3}
+console.log(res);
+export default findContinuousChar1;
---
+++ dist/continuous-char.test.js
@@ -0,0 +1,25 @@
+/**
+ * 获取字符串中连续最多的字符以及次数 test
+ */
+import findContinuousChar1 from './continuous-char';
+describe('获取字符串中连续最多的字符以及次数', () => {
+    it('正常情况', () => {
+        const str = 'aabbccddeeee11223';
+        const res = findContinuousChar1(str);
+        expect(res).toEqual({ char: 'e', length: 4 });
+    });
+    it('空字符串', () => {
+        const res = findContinuousChar1('');
+        expect(res).toEqual({ char: '', length: 0 });
+    });
+    it('无连续字符', () => {
+        const str = 'abc';
+        const res = findContinuousChar1(str);
+        expect(res).toEqual({ char: 'a', length: 1 });
+    });
+    it('全部都是连续字符', () => {
+        const str = 'aaa';
+        const res = findContinuousChar1(str);
+        expect(res).toEqual({ char: 'a', length: 3 });
+    });
+});
